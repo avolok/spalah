f9a2911f98113c2f248d26dd63930f7f2164ff75
Alex Volok <olexandr.volok@asml.com>
2026-01-17 18:09:49 +0100

    fix: update target_schema argument to accept DataFrame in exception test

--- tests/test_SchemaComparer.py
+++ tests/test_SchemaComparer.py
@@ -74,7 +74,7 @@ def test_SchemaComparer_compare_exception_passed_dataframe(
     with pytest.raises(Exception) as e:
         schema_comparer = SchemaComparer(
             source_schema=df1.schema,
-            target_schema=df1.schema,  # Dataframe passed instead of StructType (schema)
+            target_schema=df1,  # Dataframe passed instead of StructType (schema)  # ty:ignore[invalid-argument-type]
         )
         schema_comparer.compare()
     assert str(e.value).startswith(
